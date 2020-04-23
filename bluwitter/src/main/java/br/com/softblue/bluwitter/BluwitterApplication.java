2b46c4d3529e80a36ad4ae36eafce2c255c4adfa
wendey <wendeyricardo@gmail.com>
2020-04-22 22:34:07 -0300

    criando o projeto

---
+++ bluwitter/src/main/java/br/com/softblue/bluwitter/BluwitterApplication.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluwitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluwitterApplication.class, args);
+	}
+
+}
---
+++ bluwitter/src/test/java/br/com/softblue/bluwitter/BluwitterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softblue.bluwitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BluwitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
